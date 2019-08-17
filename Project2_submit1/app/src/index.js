c1d1e1c48d4377de83951663b9327f1befcd03f9
Rina <babycoco.rina@gmail.com>
2019-08-17 11:44:19 +0900

    Update index.js

--- Project2_submit1/app/src/index.js
+++ Project2_submit1/app/src/index.js
@@ -41,9 +41,10 @@ const App = {
 
   // Implement Task 4 Modify the front end of the DAPP
   lookUp: async function (){
+    const { lookUptokenIdToStarInfo } = this.meta.methods;
     const lookUpId = document.getElementById("lookid").value;
-    await lookUptokenIdToStarInfo(lookUpId).send({from: this.account});
-    App.setStatus("Your registered star is" + ".");
+    const starName = await lookUptokenIdToStarInfo(lookUpId).call;
+    App.setStatus("Your registered star is" + starName + ".");
   }
 };
 
